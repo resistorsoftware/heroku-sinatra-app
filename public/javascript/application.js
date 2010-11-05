f59a0c241089e6942984c757c0e92d11bccf34fc
David Lazar <dlazar@dlazar-laptop.(none)>
2010-11-05 14:13:09 -0400

    added event handlers to form

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,10 @@
 $(function () {
     console.log("DOM Ready");
+    $("#submit_entry").click(function(e){
+      e.preventDefault();
+      if($("#add_entry").val().length > 0) {
+        console.log("You will submit ", $("#add_entry").val());
+      }
+
+    });
 });
